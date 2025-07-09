3ab0353e2d3e9f0577e48a0eb0e4bc75de85db14
ederzin13 <edermurilo131313@gmail.com>
2025-07-09 11:14:24 -0300

    primeiras migrations - talvez precise de ajustes no futuro

---
+++ database/migrations/2025_07_09_134855_create_cart_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("cartId")->references("id")->on("carts");
+            $table->foreignId("productId")->references("id")->on("products");
+            $table->integer("quantity");
+            $table->decimal("unitPrice", 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
