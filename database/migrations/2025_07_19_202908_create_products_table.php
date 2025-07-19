ccd8f867c0a8e537d109816d443d37242100cd89
ederzin13 <edermurilo131313@gmail.com>
2025-07-19 18:02:36 -0300

    fix migrations

--- database/migrations/2025_07_19_202908_create_products_table.php
+++ database/migrations/2025_07_19_202908_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("categoryId")->references("id")->on("cart_items");
+            $table->foreignId("categoryId")->references("id")->on("categories");
             $table->string("name");
             $table->integer("stock");
             $table->decimal("price", 10, 2);
